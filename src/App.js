4b2be367884b86151272b8e62d3e1b4a490a0e7a
jcolejeff <ikwuhj@gmail.com>
2022-05-26 16:21:14 +0100

    setup exports from pages folder from index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <h4>comfy sloth starter</h4>;
 }
 
-export default App
+export default App;
