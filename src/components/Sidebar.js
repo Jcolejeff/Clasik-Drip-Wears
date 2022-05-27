b59e31124c60fdf28f75f3a5bbc4f96d96b20940
jcolejeff <ikwuhj@gmail.com>
2022-05-27 18:45:50 +0100

    implenmented sidebar toogle for all pages

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,15 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const isOpen = true;
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : " sidebar"}`}>
+      <aside
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : " sidebar"}`}
+      >
         <div className="sider-header">
           <img src={logo} alt="" />
-          <button type="button" className="close-btn">
+          <button type="button" className="close-btn" onClick={closeSidebar}>
             <FaTimes></FaTimes>
           </button>
         </div>
@@ -24,12 +26,16 @@ const Sidebar = () => {
             const { id, text, url } = link;
             return (
               <li key={id}>
-                <Link to={url}>{text}</Link>
+                <Link onClick={closeSidebar} to={url}>
+                  {text}
+                </Link>
               </li>
             );
           })}
           <li>
-            <Link to="/checkout">checkout</Link>
+            <Link to="/checkout" onClick={closeSidebar}>
+              checkout
+            </Link>
           </li>
         </ul>
         <CartButtons></CartButtons>
