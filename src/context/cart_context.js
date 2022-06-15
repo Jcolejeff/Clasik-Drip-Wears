f9ba2f47dbb09eb8e6d176eba520eacd84fd9ae2
jcolejeff <ikwuhj@gmail.com>
2022-06-15 15:32:43 +0100

    added cart functionality

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -35,16 +35,24 @@ export const CartProvider = ({ children }) => {
 		});
 	};
 	// remove from cart
-	const removeItem = (id) => {};
+	const removeItem = (id) => {
+		dispatch({ type: REMOVE_CART_ITEM, payload: id });
+	};
 	// toggle item amount
-	const toggleAmount = (id, value) => {};
+	const toggleAmount = (id, value) => {
+		dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } });
+	};
 
 	// clear cart
-	const clearCart = () => {};
+	const clearCart = () => {
+		dispatch({ type: CLEAR_CART });
+	};
 
 	useEffect(() => {
+		dispatch({ type: COUNT_CART_TOTALS });
 		localStorage.setItem("cart", JSON.stringify(state.cart));
 	}, [state.cart]);
+
 	return (
 		<CartContext.Provider
 			value={{ ...state, addToCart, toggleAmount, clearCart, removeItem }}
